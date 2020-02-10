32e1b6a36c1a0ad80950ba8198dc01a37dcc0a8d
Jose Alvaro Martínez Romero <alvaromashiro@gmail.com>
2020-02-10 16:17:03 -0600

    Revert "DevelopmentER"

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,52 +1,7 @@
-const path = require(`path`)
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/node-apis/
+ */
 
-exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
-
-  return graphql(
-    `
-      {
-        doctors: allContentfulResearchers {
-          edges {
-            node {
-              id
-              name
-              slug
-              picture {
-                file {
-                  url
-                }
-              }
-            }
-          }
-        }
-      }
-    `
-  )
-    .then(result => {
-      if (result.errors) {
-        console.log(
-          "Error retrieving contentful data",
-          JSON.stringify(result.errors)
-        )
-      }
-
-      const doctorPostTemplate = path.resolve("./src/templates/doctors.js")
-
-      result.data.doctors.edges.forEach(({ node }) => {
-        createPage({
-          path: `/investigacion/${node.slug}/`,
-          component: doctorPostTemplate,
-          context: {
-            slug: node.slug,
-            id: node.id,
-            name: node.name,
-            picture: node.picture.file.url,
-          },
-        })
-      })
-    })
-    .catch(error => {
-      console.log("Error retrieving contentful data", error)
-    })
-}
+// You can delete this file if you're not using it
--- src/pages/investigacion/index.js
+++ src/pages/investigacion/index.js
@@ -1,64 +1,50 @@
-import React from "react"
-import SectionTitle from "../../components/SectionTitle"
-import Layout from "../../components/layout"
-import SEO from "../../components/seo"
-import DoctorCard from "../../components/DoctorCard"
+import React from "react";
+import SectionTitle from "../../components/SectionTitle";
+import Layout from "../../components/layout";
+import SEO from "../../components/seo";
+import DoctorCard from "../../components/DoctorCard";
 
-import { graphql } from "gatsby"
+import rogelio from "../../images/DrRogelio.png";
+import monica from "../../images/DraMonica.png";
+import erendira from "../../images/DraErendira.png";
+import miguel from "../../images/DrMiguel.png";
 
-const InvestigationPage = ({ data }) => {
-  const researchers = data.doctors.edges
+const InvestigationPage = () => (
+  <Layout>
+    <SEO title="Investigación" />
+    <div className="content-body investigation-page">
+      <SectionTitle title="Investigación" />
+      <p>
+        Una de nuestros objetos sociales es realizar investigación, lo cual nos permite estar a la vanguardia en los temas de medicina fetal, así como apoyar a nuestros médicos investigadores a crear conocimiento nuevo.
+      </p>
+      <p>
+        Conoce a nuestros Investigadores:
+      </p>
 
-  return (
-    <Layout>
-      <SEO title="Investigación" />
-      <div className="content-body investigation-page">
-        <SectionTitle title="Investigación" />
-        <p>
-          Una de nuestros objetos sociales es realizar investigación, lo cual
-          nos permite estar a la vanguardia en los temas de medicina fetal, así
-          como apoyar a nuestros médicos investigadores a crear conocimiento
-          nuevo.
-        </p>
-        <p>Conoce a nuestros Investigadores:</p>
-
-        <div className="doctors-grid">
-          {researchers.map(({ node }, index) => (
-            <DoctorCard
-              key={index}
-              img={node.picture.file.url}
-              name={node.name}
-              linkTo={`/investigacion/${node.slug}`}
-            />
-          ))}
-        </div>
+      <div className="doctors-grid">
+        <DoctorCard 
+          img={rogelio} 
+          name="Dr. Rogelio Cruz Martínez, MD, PhD."
+          linkTo="/investigacion/RogelioCruz"
+        />
+        <DoctorCard 
+          img={monica} 
+          name="Dra. Mónica Cruz Lemini, MD, PhD." 
+          linkTo="/investigacion/MonicaCruz"
+        />
+        <DoctorCard 
+          img={miguel} 
+          name="Dr. Miguel Martínez Rodríguez, MD, PhD." 
+          linkTo="/investigacion/MiguelMartinez"
+        />
+        <DoctorCard 
+          img={erendira} 
+          name="Dra. Eréndira Chávez González, MD, PhD."
+          linkTo="/investigacion/ErendiraChavez"
+        />
       </div>
-    </Layout>
-  )
-}
-export default InvestigationPage
+    </div>
+  </Layout>
+)
 
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    doctors: allContentfulResearchers(
-      sort: { fields: [createdAt], order: DESC }
-    ) {
-      edges {
-        node {
-          name
-          slug
-          picture {
-            file {
-              url
-            }
-          }
-        }
-      }
-    }
-  }
-`
+export default InvestigationPage;
