34b40b4b0464168200164ec049491d8c6bf88485
Osmar Ramírez <osmar.roramas@gmail.com>
2020-05-04 18:34:04 -0500

    Added new formats to events and support page

--- src/pages/apoya.js
+++ src/pages/apoya.js
@@ -6,10 +6,12 @@ import { graphql } from "gatsby";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import facebook from "../images/facebook.svg"
 import instagram from "../images/instagram.svg"
+import Institutions from "../components/landing/Institutions";
 
 const SupportPage = ({ data }) => {
   const datos = data.apoya.edges;
   const redes = data.redes.edges;
+  const convenios = data.convenios.edges
   console.log(redes);
   return (
     <Layout>
@@ -21,10 +23,12 @@ const SupportPage = ({ data }) => {
             <p key={index}>
               {documentToReactComponents(node.descripcion.json)}
             </p>
-            <p><b>Nombre de la fundación: </b>{node.nombreFundacion}</p>
-            <p><b>Banco: </b>{node.banco}</p>
-            <p><b>No. Cuenta: </b>{node.cuenta}</p>
-            <p><b>CLABE: </b>{node.clabe}</p>
+            <div className="donations-card">
+              <p><b>Nombre de la fundación: </b>{node.nombreFundacion}</p>
+              <p><b>Banco: </b>{node.banco}</p>
+              <p><b>No. Cuenta: </b>{node.cuenta}</p>
+              <p><b>CLABE: </b>{node.clabe}</p>
+            </div>
             <p>
               ¡Contáctanos! Escribe un mail a <a className="secondary-link" href={`mailto:${node.correo}?Subject=Nueva%20Pregunta`}>{node.correo}</a> o llámanos al {node.telefono}.
           </p>
@@ -34,16 +38,16 @@ const SupportPage = ({ data }) => {
 
         <div className="row button-container">
           {datos.map(({ node }, index) => (
-            <div key={index}>
+            <>
               <a className="button button-cta" href={`mailto:${node.correo}?Subject=Nueva%20Pregunta`}>Enviar Correo Electrónico</a>
               <a className="button button-cta" href={`tel:${node.telefono}`}>Llamar vía Teléfono</a>
-            </div>
+            </>
           ))}
         </div>
         <br />
         <p>
           No olvides de seguirnos en nuestras redes sociales:
-      </p>
+        </p>
         {redes.map(({ node }) => (
           <div className="social-media-container">
             <a href={`${node.facebook}`}>
@@ -54,6 +58,17 @@ const SupportPage = ({ data }) => {
             </a>
           </div>
         ))}
+        <br/><br/>
+        <SectionTitle title="Nuestros Patrocinadores" />
+        <div className="images-container">
+          {convenios.map(({ node }, index) => (
+            <div key={index}>
+              <Institutions
+                image={node.imagen.file.url}
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </Layout>
   )
@@ -75,6 +90,18 @@ export const query = graphql`
           clabe
           cuenta
           nombreFundacion
+        }
+      }
+    }
+    convenios: allContentfulConvenios {
+      edges {
+        node {
+          imagen {
+            file {
+              url
+            }
+          }
+          organizacin
         }
       }
     }
--- src/pages/eventos.js
+++ src/pages/eventos.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { graphql } from "gatsby"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+
 import SectionTitle from "../components/SectionTitle";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import { graphql } from "gatsby"
-import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+
+import calendar from "../images/calendar.svg";
+import location from "../images/location.svg";
+
 const EventsPage = ({ data }) => {
   const datos = data.eventos.edges
   console.log(datos)
@@ -13,13 +18,22 @@ const EventsPage = ({ data }) => {
       <div className="content-body events-page">
         <SectionTitle title="Próximos eventos" />
         {datos.map(({ node }, index) => (
-          <>
-            <h3>{node.titulo}</h3>
-            <h4>{node.fecha}</h4>
-            <h4>{node.lugar}</h4>
-            <>{documentToReactComponents(node.descripcion.json)}</>
-            <SectionTitle />
-          </>
+          <div className="event-card">
+            <div className="circle-up" />
+            <div className="box">
+              <h3>{node.titulo}</h3>
+              <div className="line">
+                <img src={calendar} alt=""/>
+                <h4>{node.fecha}</h4>
+              </div>
+              <div className="line">
+                <img src={location} alt=""/>
+                <h4>{node.lugar}</h4>
+              </div>
+              <>{documentToReactComponents(node.descripcion.json)}</>
+            </div>
+            <div className="circle-bot" />
+          </div>
         ))}
       </div>
     </Layout>
