a0085b6ef30ced008cefb2de22d8f8779e45a392
EmmanuelER <emmanuel@neuronmexico.com>
2020-05-18 13:24:05 -0500

    added Investigation Articles

--- src/pages/investigacion/index.js
+++ src/pages/investigacion/index.js
@@ -3,21 +3,32 @@ import SectionTitle from "../../components/SectionTitle"
 import Layout from "../../components/layout"
 import SEO from "../../components/seo"
 import DoctorCard from "../../components/DoctorCard"
-
-import { graphql } from "gatsby"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import { graphql, Link } from "gatsby"
 
 const InvestigationPage = ({ data }) => {
   const researchers = data.doctors.edges
-
+  const articles = data.articles.edges
   return (
     <Layout>
       <SEO title="Investigación" />
       <div className="content-body investigation-page">
         <SectionTitle title="Investigación" />
         <p>
-        En Fundación Medicina Fetal México, la investigación es uno de nuestros pilares más grandes y de mayor importancia, debido a que gracias a nuestra producción científica, los médicos investigadores de nuestro grupo son referentes a nivel mundial. Han creado nuevas técnicas de tratamiento siendo pioneros en algunas intervenciones fetales, demostrando también su empatía con las mujeres embarazadas de nuestro país.
+          En Fundación Medicina Fetal México, la investigación es uno de
+          nuestros pilares más grandes y de mayor importancia, debido a que
+          gracias a nuestra producción científica, los médicos investigadores de
+          nuestro grupo son referentes a nivel mundial. Han creado nuevas
+          técnicas de tratamiento siendo pioneros en algunas intervenciones
+          fetales, demostrando también su empatía con las mujeres embarazadas de
+          nuestro país.
         </p>
-        <p>Día con día ponen todo su empeño en mejorar la medicina fetal de México y el mundo, para poder ofrecer más y mejores opciones a nuestras pacientes, para que todos los niños lleguen a desarrollar su máximo potencial, por un mundo mejor.</p>
+        <p>
+          Día con día ponen todo su empeño en mejorar la medicina fetal de
+          México y el mundo, para poder ofrecer más y mejores opciones a
+          nuestras pacientes, para que todos los niños lleguen a desarrollar su
+          máximo potencial, por un mundo mejor.
+        </p>
 
         <div className="doctors-grid">
           {researchers.map(({ node }, index) => (
@@ -28,6 +39,20 @@ const InvestigationPage = ({ data }) => {
               linkTo={`/investigacion/${node.slug}`}
             />
           ))}
+        </div>
+        <SectionTitle title="Artículos de Investigación" />
+        <div className="investigation-articles">
+          <ul>
+            {articles.map(({ node }, index) => (
+              <li key={index}>
+                <a href={node.url}>
+                  {node.titulo}
+                  {". "}
+                </a>
+                {documentToReactComponents(node.descripcion.json)}
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </Layout>
@@ -54,6 +79,17 @@ export const pageQuery = graphql`
               url
             }
           }
+        }
+      }
+    }
+    articles: allContentfulInvestigationArticles {
+      edges {
+        node {
+          titulo
+          descripcion {
+            json
+          }
+          url
         }
       }
     }
