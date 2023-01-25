07738c55bb10c36cab70adf52ab5f99670fc018d
Von Valentine <73485243+WinniReyna@users.noreply.github.com>
2023-01-25 13:18:25 -0600

    add counter

--- src/components/landing/NewEvent.js
+++ src/components/landing/NewEvent.js
@@ -1,47 +1,48 @@
 import { Link, StaticQuery, graphql } from "gatsby"
-import React, { Component, useState} from "react"
+import React, { Component, useState } from "react"
 import newEvent from "../../images/eventNew.jpeg"
+import DateCountdown from 'react-date-countdown-timer'
 
 class NewEvent extends Component {
-    
-    constructor(props) {
-        super(props)
 
-        this.imageBox = null
-        this.image = null
-        this.textBox = null
-        this.title = null
-        this.header = null
-        this.cta = null
-        this.socialMedia = null
+  constructor(props) {
+    super(props)
 
-        //this.animation = new TimelineLite({ paused: true })
-    }
-    componentDidMount() {
-        // this.animation
-        //   .from(this.textBox, 1, { x: 200, width: 0 }, 1)
-        //   .from(this.imageBox, 0.75, { width: 0 }, 2)
-        //   .from(this.title, 0.5, { autoAlpha: 0, opacity: 0, y: -8 }, 3)
-        //   .from(this.header, 0.5, { autoAlpha: 0, opacity: 0, y: -8 }, 3.8)
-        //   .from(this.cta, 0.7, { autoAlpha: 0, opacity: 0 }, 4.25)
-        //   .from(this.socialMedia, 1, { opacity: 0, y: 20 }, 4.5)
-        //this.animation.play()
-    }
+    this.imageBox = null
+    this.image = null
+    this.textBox = null
+    this.title = null
+    this.header = null
+    this.cta = null
+    this.socialMedia = null
 
-    render() {
-        
-        
-        const Span = () => (
-            <span
-                style={{
-                    display: "none",
-                }}
-            ></span>
-        )
+    //this.animation = new TimelineLite({ paused: true })
+  }
+  componentDidMount() {
+    // this.animation
+    //   .from(this.textBox, 1, { x: 200, width: 0 }, 1)
+    //   .from(this.imageBox, 0.75, { width: 0 }, 2)
+    //   .from(this.title, 0.5, { autoAlpha: 0, opacity: 0, y: -8 }, 3)
+    //   .from(this.header, 0.5, { autoAlpha: 0, opacity: 0, y: -8 }, 3.8)
+    //   .from(this.cta, 0.7, { autoAlpha: 0, opacity: 0 }, 4.25)
+    //   .from(this.socialMedia, 1, { opacity: 0, y: 20 }, 4.5)
+    //this.animation.play()
+  }
 
-        return (
-            <StaticQuery
-                query={graphql`
+  render() {
+
+
+    const Span = () => (
+      <span
+        style={{
+          display: "none",
+        }}
+      ></span>
+    )
+
+    return (
+      <StaticQuery
+        query={graphql`
           {
             Redes: allContentfulRedesSociales {
               edges {
@@ -65,28 +66,34 @@ class NewEvent extends Component {
             }
           }
         `}
-                render={data => (
-                    <>
-                        <section className="about">
-                            <div className="content-screen">
-                                <div className="row">
-                                    <img src={newEvent} alt="" className="image-content" />
-                                    <div className="column content-box">
-                                        <h1 ref={h1 => (this.header = h1)} >¡DI SÍ AL REDONDEO EN TIENDAS OXXO!</h1>
-                                        <p>
-                                            De enero a marzo de 2023 cada redondeo que hagas en tiendas Oxxo de Zapopan ayudarás con la compra de materiales e insumos médicos necesarios en las cirugías fetales y terapias neonatales.
-                                            Tu aportación ayuda a mamás y bebés en vida fetal
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </section>
-                        
-                    </>
-                )}
-            />
-        )
-    }
+        render={data => (
+          <>
+            <section className="about">
+              <div className="content-screen">
+                <div className="row">
+                  <img src={newEvent} alt="" className="image-content" />
+                  <div className="column content-box">
+                    <br></br>
+                    <h1 ref={h1 => (this.header = h1)} >¡DI SÍ AL REDONDEO EN TIENDAS OXXO!</h1>
+                    <p>
+                      De enero a marzo de 2023 cada redondeo que hagas en tiendas Oxxo de Zapopan ayudarás con la compra de materiales e insumos médicos necesarios en las cirugías fetales y terapias neonatales.
+                      Tu aportación ayuda a mamás y bebés en vida fetal
+                    </p>
+
+                    <h1 style={{"color": "#f5b4c8"}}>
+                    <DateCountdown dateTo='March 31, 2023 24:00:00 GMT+03:00' mostSignificantFigure='day' numberOfFigures='1'/>
+                    </h1>
+
+                  </div>
+                </div>
+              </div>
+            </section>
+
+          </>
+        )}
+      />
+    )
+  }
 }
 
 
