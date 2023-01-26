35992bf50a9f455e7ad20f04487c579dec0c761e
Von Valentine <73485243+WinniReyna@users.noreply.github.com>
2023-01-26 07:35:21 -0600

    Update NewEvent.js

--- src/components/landing/NewEvent.js
+++ src/components/landing/NewEvent.js
@@ -7,19 +7,18 @@ function NewEvent() {
   const endDate = new Date('March 31, 2023 23:59:59');
   var duration = endDate.getTime() - now.getTime();
   var  difference = duration / (1000 * 3600 * 24);
+  const [date, setDate] = useState(difference);
   const [hide, setHide] = useState("");
   
   useEffect(() => {   
 
     if(now < endDate){
+      console.log(date | 0);
       setHide("");
     }
     else{
       setHide("none");
     }
-    
-
-    console.log(difference);
   }, [difference])
 
   return (
@@ -35,13 +34,11 @@ function NewEvent() {
                 De enero a marzo de 2023 cada redondeo que hagas en tiendas Oxxo de Zapopan ayudarás con la compra de materiales e insumos médicos necesarios en las cirugías fetales y terapias neonatales.
                 Tu aportación ayuda a mamás y bebés en vida fetal.
               </p>
-
-              <h3 style={{"color": "#f5b4c8", "display": hide}}>{difference.toFixed(0)} días restantes.</h3>
+              <h3 style={{"color": "#f5b4c8", "display": hide}}>{date | 0} días restantes.</h3>
             </div>
           </div>
         </div>
       </section>
-
     </>
   )
 }
